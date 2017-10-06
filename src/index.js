abadac47e02feabcc38ad6e1d9d32bd952cb7165
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-05 23:34:49 -0700

    Add redux store to keep central repository of state

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {createStore} from 'redux';
+
+const defaultState = { checked: false }
+const reducer = function(state = defaultState, action) {
+    switch(action.type) {
+        case 'TOGGLE':
+            return { ...state, checked: !state.checked };
+        default:
+            return state;
+    }
+}
+const store = createStore(reducer);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
