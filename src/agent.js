ce43f0740df40bb2f4b98e64503311796ab9c828
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-06 18:13:43 -0700

    Add superagent for pulling from REST APIs

---
+++ src/agent.js
@@ -0,0 +1,17 @@
+import superagent from 'superagent';
+
+const API_ROOT = 'http://conduit.productionready.io/api';
+
+const responseBody = res => res.body;
+
+const requests = {
+    get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody)
+}
+
+const Articles = {
+    all: page => requests.get('articles?limit=10')
+}
+
+export default {
+    Articles
+}
