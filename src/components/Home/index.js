10369e7c95e38d684d4361b86164e0f90a003d30
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-08 23:15:06 -0700

    Add middleware to handle getting data from a promise that is returned when calling the API

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import agent from '../../agent';
+
 import Banner from './Banner';
 import MainView from './MainView';
 
--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {promiseMiddleware} from './middleware';
 
 const defaultState = { appName: 'CONDUIT' };
 const reducer = function(state = defaultState, action) {
     switch(action.type) {
-        case 'TOGGLE':
-            return { ...state, checked: !state.checked };
+        case 'HOME_PAGE_LOADED':
+            return { ...state, articles: action.payload.articles };
         default:
             return state;
     }
 }
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
