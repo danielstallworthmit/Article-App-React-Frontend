c9af612e86c7da35d9eb76c212199fd4d048d1a2
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-09 20:41:59 -0700

    Move store to its own file to declutter index file

--- src/index.js
+++ src/index.js
@@ -4,20 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
-import {promiseMiddleware} from './middleware';
+import store from './store';
 
-const defaultState = { appName: 'CONDUIT' };
-const reducer = function(state = defaultState, action) {
-    switch(action.type) {
-        case 'HOME_PAGE_LOADED':
-            return { ...state, articles: action.payload.articles };
-        default:
-            return state;
-    }
-}
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
