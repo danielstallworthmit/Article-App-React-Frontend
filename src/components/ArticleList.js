2e532c36b7e06f54e27a82a4aec0644101e29578
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-09 20:26:42 -0700

    Add ArticlePreview to show a preview of each article in the article list

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArticlePreview from './ArticlePreview';
 
 const ArticleList = props => {
     if (!props.articles) {
@@ -18,7 +19,7 @@ const ArticleList = props => {
             {
                 props.articles.map(article => {
                     return (
-                        <h2>{article.title}</h2>
+                        <ArticlePreview article={article} key={article.slug} />
                     );
                 })
             }
