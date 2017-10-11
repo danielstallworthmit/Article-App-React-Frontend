b1f4fb515278575f3ec4c0dfcbbd36abf8f74aa9
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-11 09:38:50 -0700

    Add errors for sign in page and dispatch actions for auth reducer

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import agent from '../agent';
+import ListErrors from './ListErrors';
+
+const mapStateToProps = state => ({ ...state.auth });
+const mapDispatchToProps = dispatch => ({
+    onChangeEmail: value => dispatch({type: 'UPDATE_FIELD_AUTH', key: 'email', value }),
+    onChangePassword: value => dispatch({type: 'UPDATE_FIELD_AUTH', key: 'password', value }),
+    onSubmit: (email, password) => dispatch({type: 'LOGIN', payload: agent.Auth.login(email, password) })
+});
 
 class Login extends React.Component {
+    constructor() {
+        super();
+        this.changeEmail = ev => this.props.onChangeEmail(ev.target.value);
+        this.changePassword = ev => this.props.onChangePassword(ev.target.value);
+        this.submitForm = (email, password) => ev => {
+            ev.preventDefault();
+            this.props.onSubmit(email, password);
+        }
+    }
     render() {
+        const {email, password} = this.props;
         return (
             <div className="auth-page">
                 <div className="container page">
@@ -14,20 +33,26 @@ class Login extends React.Component {
                                     Need an account?
                                 </a>
                             </p>
-                            <form>
+                            <ListErrors errors={this.props.errors} />
+                            <form onSubmit={this.submitForm(email, password)}>
                                 <fieldset>
                                     <fieldset className="form-group">
                                         <input className="form-control form-control-lg"
                                             type="email"
-                                            placeholder="Email" />
+                                            placeholder="Email"
+                                            value={email}
+                                            onChange={this.changeEmail} />
                                     </fieldset>
                                     <fieldset className="form-group">
                                         <input className="form-control form-control-lg"
                                             type="password"
-                                            placeholder="Password" />
+                                            placeholder="Password"
+                                            value={password}
+                                            onChange={this.changePassword} />
                                     </fieldset>
                                     <button className="btn btn-lg btn-primary pull-xs-right"
-                                        type="submit">
+                                        type="submit"
+                                        disabled={this.props.inProgress} >
                                         Sign In
                                     </button>
                                 </fieldset>
@@ -40,4 +65,4 @@ class Login extends React.Component {
     }
 }
 
-export default connect(()=> ({}))(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,3 +1,19 @@
 export default (state={}, action) => {
-    return state;
+    switch(action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                inProgress: false,
+                errors: action.error ? action.payload.errors : null
+            }
+        case 'UPDATE_FIELD_AUTH':
+            return { ...state, [action.key]: action.value }
+        case 'ASYNC_START':
+            if (action.subtype === 'LOGIN') {
+                return { ...state, inProgress: true }
+            }
+            return state;
+        default:
+            return state;
+    }
 }
