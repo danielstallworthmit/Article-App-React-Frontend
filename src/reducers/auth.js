4a49706069529e554a2469476e66a433b05c481e
danielstallworthmit <danielstallworthmit@gmail.com>
2017-10-11 22:01:05 -0700

    Add unmounting of state on logout

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,10 +7,14 @@ export default (state={}, action) => {
                 inProgress: false,
                 errors: action.error ? action.payload.errors : null
             }
+        case 'LOGIN_PAGE_UNLOADED':
+            return {};
         case 'UPDATE_FIELD_AUTH':
             return { ...state, [action.key]: action.value }
+        case 'UPDATE_FIELD_AUTH':
+            return {...state, [action.key]: action.value}
         case 'ASYNC_START':
-            if (action.subtype === 'LOGIN') {
+            if (action.subtype === 'LOGIN' || action.subtype === 'REGISTER') {
                 return { ...state, inProgress: true }
             }
             return state;
